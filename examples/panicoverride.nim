c90f37ca5ccc3111da7a17f18931c547c472d091
Jacques Wagener <jacques@dilectum.co.za>
2019-06-25 14:22:35 +0200

    Add revert as panicoverride :)

--- examples/panicoverride.nim
+++ examples/panicoverride.nim
@@ -1,4 +1,6 @@
 {.push stack_trace: off, profiler:off.}
-proc rawoutput(s: string) = discard
+proc revert(dataOffset: pointer; length: int32) {.noreturn, cdecl, importc.}
+proc rawoutput(s: string) = 
+    revert(cstring(s), s.len.int32)
 proc panic(s: string) = rawoutput(s)
 {.pop.}
