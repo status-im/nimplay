7a85fd40766b1401a85f5b0229971bad6c5754fa
Jacques Wagener <jacques@dilectum.co.za>
2019-08-15 23:16:28 +0200

    Fix exporting of functions.

--- examples/panicoverride.nim
+++ examples/panicoverride.nim
@@ -1,7 +1,6 @@
 {.push stack_trace: off, profiler:off.}
 proc revert(dataOffset: pointer; length: int32) {.noreturn, cdecl, importc.}
 proc rawoutput(s: string) = 
-    # revert(cstring(s), s.len.int32)
-    revert(unsafeAddr s[0], s.len.int32)
+    revert(cstring(s), s.len.int32)
 proc panic(s: string) = rawoutput(s)
 {.pop.}
--- tools/eth_postprocess.sh
+++ tools/eth_postprocess.sh
@@ -12,8 +12,6 @@ fi
 # Replace "env" with "ethereum"
 wasm2wat "$WASM_FILE" |
  sed 's/(import "env" /(import "ethereum" /g' |
- sed 's/(export "__heap_base" (global 1))//g' |
- sed 's/(export "__data_end" (global 2))//g' > /tmp/wasm.tmp
+ sed  '/(export.*memory\|main.*/! s/(export.*//g' > /tmp/wasm.tmp
 
 wat2wasm -o "$WASM_FILE" /tmp/wasm.tmp
-
